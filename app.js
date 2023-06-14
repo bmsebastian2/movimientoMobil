625963d855927a3148aac21827f2ff3ed5b47227
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-06-14 15:47:36 -0300

    s3

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hola");
+console.log("hola 2");
+window.addEventListener("deviceorientation", handleOrientation, true);
+
+function handleOrientation(event) {
+  const absolute = event.absolute;
+  const alpha = event.alpha;
+  const beta = event.beta;
+  const gamma = event.gamma;
+  console.log(alpha);
+  // Do stuff with the new orientation data
+}
