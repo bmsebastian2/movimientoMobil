d153fdb88ed9c3bdd85c6b73b140a7d3e5820451
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-06-14 16:31:53 -0300

    s10

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ let h1 = document.getElementById("h1");
 if (window.DeviceOrientationEvent) {
   window.addEventListener("deviceorientation", handleOrientation, true);
   h1.innerText = "suport";
+  function handleOrientation(event) {
+    const absolute = event.absolute;
+    const alpha = event.alpha;
+    const beta = event.beta;
+    const gamma = event.gamma;
+    console.log(alpha);
+    parrafoBeta[0].innerText = parrafoBeta[0].innerText + 1;
+    // Do stuff with the new orientation data
+  }
 }
 
-window.addEventListener("deviceorientation", handleOrientation, true);
-window.addEventListener("devicemotion", handleMotion, true);
+if (window.DeviceMotionEvent) {
+  window.addEventListener("devicemotion", handleMotion);
 
-function handleOrientation(event) {
-  const absolute = event.absolute;
-  const alpha = event.alpha;
-  const beta = event.beta;
-  const gamma = event.gamma;
-  console.log(alpha);
-  parrafoBeta[0].innerText = parrafoBeta[0].innerText + 1;
-  // Do stuff with the new orientation data
-}
-
-function handleMotion(event) {
-  const absolute = event.absolute;
-  const alpha = event.alpha;
-  const beta = event.beta;
-  const gamma = event.gamma;
-  console.log(alpha);
-  parrafoBeta[1].innerText = beta;
-  // Do stuff with the new orientation data
+  function handleMotion(event) {
+    const absolute = event.absolute;
+    const alpha = event.alpha;
+    const beta = event.beta;
+    const gamma = event.gamma;
+    console.log(alpha);
+    parrafoBeta[1].innerText = beta;
+    // Do stuff with the new orientation data
+  }
 }
