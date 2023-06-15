37c6a0d9f6720be8d0c5f3084e8076dd43bd9657
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-06-15 19:31:46 -0300

    www

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ if (window.DeviceMotionEvent) {
     const rotationY = ((ay - halfHeight) / halfHeight) * 30;
     // const rotationZ = ((az - halfHeight) / halfHeight) * 30;
 
-    ticket.style.transform = `rotateX(${ax * 10}deg) rotateY(${ay * 10}deg)`;
+    ticket.style.transform = `rotateX(${
+      event.accelerationIncludingGravity.x * 5
+    }deg) rotateY(${event.accelerationIncludingGravity.y * 5}deg)`;
     if (ax === 0 && ay === 0) {
       ticket.style.transition = "transform .5s ease-in-out";
       ticket.style.transform = `rotateX(0deg) rotateY(0deg)`;
