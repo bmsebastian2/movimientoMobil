87ef4af0489c3fb62bcaa100f45dee56561ada92
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-06-15 18:21:39 -0300

    Prueba

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-console.log("hola 3");
 let parrafoBeta = document.getElementsByClassName("beta");
 let h1 = document.getElementById("h1");
 
-if (window.DeviceOrientationEvent) {
-  window.addEventListener("deviceorientation", handleOrientation, true);
-  h1.innerText = "suport";
-  function handleOrientation(event) {
-    parrafoBeta[0].innerText = "Beta: " + event.gamma;
-  }
-}
+// if (window.DeviceOrientationEvent) {
+//   window.addEventListener("deviceorientation", handleOrientation, true);
+//   h1.innerText = "suport";
+//   function handleOrientation(event) {
+//     parrafoBeta[0].innerText = "Beta: " + event.gamma;
+//   }
+// }
 
-if (window.DeviceMotionEvent) {
-  window.addEventListener("devicemotion", handleMotion);
+// if (window.DeviceMotionEvent) {
+window.addEventListener("devicemotion", handleMotion);
 
-  function handleMotion(event) {
-    const absolute = event.absolute;
-    const alpha = event.alpha;
-    const beta = event.beta;
-    const gamma = event.gamma;
-    console.log(alpha);
-    parrafoBeta[1].innerText = "aplha" + alpha;
-    // Do stuff with the new orientation data
-  }
+function handleMotion(event) {
+  // const absolute = event.absolute;
+  const ax = event.accelerationIncludingGravity.x;
+  const ay = event.accelerationIncludingGravity.y;
+  const az = event.accelerationIncludingGravity.z;
+  parrafoBeta[0].innerText = ax + ay + az;
+  // console.log(alpha);
+  // parrafoBeta[1].innerText = "aplha" + alpha;
+  // Do stuff with the new orientation data
 }
+// }
